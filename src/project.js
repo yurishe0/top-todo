3196d58cdbd3fe6e5d4e355eaff0af270e382df2
yurishe0 <yurishe00@gmail.com>
2023-07-24 18:55:48 +0200

    Add new methods to the 'Project' class

--- src/project.js
+++ src/project.js
@@ -3,4 +3,16 @@ export class Project {
         this.name = name;
         this.list = [];
     }
+
+    getName() {
+        return this.name;
+    }
+
+    setName(newName) {
+        this.name = newName;
+    }
+
+    addTodo(todo) {
+        this.list.push(todo);
+    }
 }
