a4a03ec4da0d7f4a8ae8e5f719bf203024a3a749
yurishe0 <yurishe00@gmail.com>
2023-07-27 22:43:30 +0200

    Add the logic for removing the projects from the list

--- src/storage.js
+++ src/storage.js
@@ -6,8 +6,11 @@ export const storage = (() => {
     }
 
     const removeProject = (project) => {
-        const index = projectList.indexOf(project);
-        projectList.splice(index);
+        const index = projectList.findIndex((item) => item.getName() == project.getName());
+        if(index != -1) {
+            projectList.splice(index, 1);
+            console.log('Project removed.');
+        }
     }
 
     return {
