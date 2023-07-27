cd4ee78882e5c4bbf26ae684a0a80e9004419ce6
yurishe0 <yurishe00@gmail.com>
2023-07-27 22:43:00 +0200

    Structure modification

--- src/createProject.js
+++ src/createProject.js
@@ -1,28 +1,10 @@
 import { Project } from './project.js';
-import { storage } from './storage';
+import { storage } from './storage.js';
+import { UI } from './UI.js';
 
 export const createProject = (name) => {
     const project = new Project(name);
     storage.addProject(project);
 
-    const projectList = document.querySelector('#project-list');
-    const projectContainer = document.createElement('li');
-    const projectName = document.createElement('span');
-
-    const projectEdit = document.createElement('i');
-    projectEdit.classList.add("material-icons", "edit-project");
-    projectEdit.innerHTML = "edit";
-
-    const projectDelete = document.createElement('i');
-    projectDelete.classList.add("material-icons", "delete-project");
-    projectDelete.innerHTML = "delete";
-
-    projectContainer.classList.add('project');
-    projectName.textContent = project.name;
-
-    projectContainer.appendChild(projectName);
-    projectContainer.appendChild(projectEdit);
-    projectContainer.appendChild(projectDelete);
-    projectList.prepend(projectContainer);
-
+    UI.addProjectToPage(project);
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import { createProject } from './createProject.js';
 
-const addProject = document.querySelector("#project-adder");
-const button2 = document.querySelector("#test2");
-
-addProject.addEventListener('click', () => {
-    createProject("Test project");
-});
