f18a04b604cb5ef6e92a652edcd10b17e4123259
yurishe0 <yurishe00@gmail.com>
2023-07-31 00:48:32 +0200

    Add functionality for managing the project objects and nodes

--- src/storage.js
+++ src/storage.js
@@ -1,20 +1,33 @@
 export const projectList = [];
+export const nodeList = [];
 
-export const storage = (() => {
-    const addProject = (project) => {
+export class Storage {
+    static addProject(project) {
         projectList.push(project);
     }
 
-    const removeProject = (project) => {
+
+    static removeProject(project) {
         const index = projectList.findIndex((item) => item.getName() == project.getName());
         if(index != -1) {
             projectList.splice(index, 1);
-            console.log('Project removed.');
+            console.log(projectList);
         }
     }
 
-    return {
-        addProject,
-        removeProject,
+    static addNode(node) {
+        nodeList.push(node);
+        console.log("Node added. Node list status: ");
+        console.log(nodeList);
     }
-})();
+
+    static removeNode(node) {
+        const index = nodeList.findIndex((item) => item.textContent == node.textContent);
+        if(index != -1) {
+            nodeList.splice(index, 1);
+            console.log(nodeList);
+        }
+    }
+}
+
+
