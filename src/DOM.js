8cd4bbc7a8e9b02d3718153b1a0623fd6e16d14b
yurishe0 <yurishe00@gmail.com>
2023-07-31 00:49:04 +0200

    Add nodes and event listeners

--- src/DOM.js
+++ src/DOM.js
@@ -1,16 +1,14 @@
-import { createProject } from "./createProject";
+import { UI } from "./UI";
 
 export const addProject = document.querySelector("#project-adder");
 export const deleteProject = document.querySelector(".delete-project");
-
+export const body = document.querySelector('body');
+export const siteContainer = document.querySelector('.site-container');
 export const projectListContainer = document.querySelector('#project-list');
 
 (function eventListeners() {
-    let num = 0;
-
     addProject.addEventListener('click', () => {
-        createProject(num);
-        num++;
+        UI.displayPopUp("projectCreation");
     });
 })();
 
