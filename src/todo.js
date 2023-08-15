b45a0c3055521c475b42c74399fc63eb61f4d0c2
yurishe0 <yurishe00@gmail.com>
2023-08-15 22:14:21 +0200

    Additional methods

--- src/todo.js
+++ src/todo.js
@@ -5,4 +5,20 @@ export class Todo {
         this.dueDate = dueDate;
         this.priority = priority;
     }
+
+    getTitle() {
+        return this.title;
+    }
+
+    getDescription() {
+        return this.description;
+    }
+
+    getDate() {
+        return this.dueDate;
+    }
+
+    getPriority() {
+        return this.priority;
+    }
 }
